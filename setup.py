6e20750f85e6da4b3624a2bec648890efed60d9a
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-05-12 20:28:17 -0400

    here's the site

--- setup.py
+++
@@ -1,6 +0,0 @@
-try:
-    from jupyterpip import cmdclass
-except:
-    import pip, importlib
-    pip.main(["install", "jupyter-pip"])
-    cmdclass = importlib.import_module("jupyterpip").cmdclass
